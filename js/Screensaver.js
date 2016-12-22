ad870e18b65713b905bd26920bf3e3cf9cf39670
bryan kennedy <bkennedy@smm.org>
2016-12-21 21:35:26 -0600

    ES6 var declaration

--- js/Screensaver.js
+++ js/Screensaver.js
@@ -39,7 +39,7 @@ function Screensaver(timeoutSeconds, videoSrc, onSleepCallback, onAwakeCallback)
 Screensaver.prototype.createVideo = function (videoSrc) {
 
   // Create video tag
-  var videoTag = '<video ' +
+  let videoTag = '<video ' +
     'id="screensaver_video" ' +
     'style="position:fixed; top:0; left:0; z-index:999;"' +
     'loop >' +
